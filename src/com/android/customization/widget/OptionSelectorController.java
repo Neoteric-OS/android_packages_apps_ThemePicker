284f444107ddcf36f87bd8ce686b40aaf77d9f59
Erfan Abdi <erfangplus@gmail.com>
2022-09-16 23:22:19 +0430

    ThemePicker: Introduce icon shape picker
    Change-Id: I60b24f0cd8e898cb6d43ad190cad99a73cb7b2bb

--- src/com/android/customization/widget/OptionSelectorController.java
+++ src/com/android/customization/widget/OptionSelectorController.java
@@ -286,6 +286,8 @@ public class OptionSelectorController<T extends CustomizationOption<T>> {
 
         Resources res = mContainer.getContext().getResources();
         mContainer.setAdapter(mAdapter);
+	mContainer.setItemAnimator(null);
+
         final int padding = res.getDimensionPixelSize(
                 R.dimen.option_tile_grid_padding_horizontal);
         final int fixWidth = res.getDimensionPixelSize(R.dimen.options_container_width);

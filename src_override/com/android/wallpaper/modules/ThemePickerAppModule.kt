1c48070a461b4e811eb8a41ada4341c14fe1d35e
Austin Wang <wangaustin@google.com>
2025-02-26 09:51:35 -0800

    Save current image uri for extended effect (2/3)
    Flag: com.android.systemui.shared.extended_wallpaper_effects
    Bug: 395943288
    Test: set extended effect wallpaper
    Change-Id: I0074fbd2de3af5aeac955ffa584d43d3057a715e

--- src_override/com/android/wallpaper/modules/ThemePickerAppModule.kt
+++ src_override/com/android/wallpaper/modules/ThemePickerAppModule.kt
@@ -44,7 +44,9 @@ import com.android.wallpaper.customization.ui.binder.ThemePickerToolbarBinder
 import com.android.wallpaper.effects.DefaultEffectsController
 import com.android.wallpaper.effects.EffectsController
 import com.android.wallpaper.module.DefaultPartnerProvider
+import com.android.wallpaper.module.DefaultRecentWallpaperManager
 import com.android.wallpaper.module.PartnerProvider
+import com.android.wallpaper.module.RecentWallpaperManager
 import com.android.wallpaper.module.WallpaperPreferences
 import com.android.wallpaper.module.logging.UserEventLogger
 import com.android.wallpaper.picker.category.domain.interactor.CategoriesLoadingStatusInteractor
@@ -202,6 +204,12 @@ abstract class ThemePickerAppModule {
     abstract fun bindWorkspaceCallbackBinder(
         impl: ThemePickerWorkspaceCallbackBinder
     ): WorkspaceCallbackBinder
+
+    @Binds
+    @Singleton
+    abstract fun bindRecentWallpaperManager(
+        impl: DefaultRecentWallpaperManager
+    ): RecentWallpaperManager
 
     companion object {
 
